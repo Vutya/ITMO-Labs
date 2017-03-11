3715da031634115a95c9b96e465a086dad0bab2b
Vutya <viktor-timohov@mail.ru>
2017-03-11 23:43:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,39 +11,39 @@ int main() {
     a.push(5);
     a.push(12);
     a.push(7);
-    a.push(7);
     a.push(3);
+    a.push(7);
     std::cout << a.top() << std::endl; // выводится вершина очереди
     a.pop();
-    std::cout << a.top() << std::endl; //выводится вершина очереди после удаления
+    std::cout << a.top() << std::endl; //выводится вершина очереди после удаления одного элемента
     a.push(19);
     a.push(15);
-    std::cout << a.top() << std::endl; // выводится вершина очереди после доавления 2-ух элементов
+    std::cout << a.top() << std::endl; // выводится вершина очереди после добавления 2-ух элементов
     a.pop();
     a.pop();
     a.pop();
-    std::cout << a.top() << std::endl;
+    std::cout << a.top() << std::endl; // выводится вершина очереди после удаления 3-ёх элементов
 
     // создаётся вектор <5,4,6,3,2,8,2,29>
-    std::vector<int> kek;
-    kek.push_back(5);
-    kek.push_back(4);
-    kek.push_back(6);
-    kek.push_back(3);
-    kek.push_back(2);
-    kek.push_back(8);
-    kek.push_back(2);
-    kek.push_back(29);
+    std::vector<int> nevector;
+    nevector.push_back(5);
+    nevector.push_back(4);
+    nevector.push_back(6);
+    nevector.push_back(3);
+    nevector.push_back(2);
+    nevector.push_back(8);
+    nevector.push_back(2);
+    nevector.push_back(29);
 
-    for (int i = 0; i < kek.size(); i++)
-        std::cout << kek [i]<< " ";
+    for (int i = 0; i < nevector.size(); i++)
+        std::cout << nevector [i]<< " ";
     std::cout << std::endl;
 
     // пирамидальная сортировка вектора
-    heapsort(kek);
+    heapsort(nevector);
 
-    for (int i = 0; i < kek.size(); i++)
-        std::cout << kek [i]<< " ";
+    for (int i = 0; i < nevector.size(); i++)
+        std::cout << nevector [i]<< " ";
 
     return 0;
 }
